20febd88194ce170df915a7a9c99d8f78a59819c
Jose Santiago <josantorres93@gmail.com>
2020-03-20 22:34:25 -0600

    changes allow input in Name & Username input fields

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import UserTable from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm';
 
 const App = () => {
   const userData = [
@@ -10,12 +11,18 @@ const App = () => {
 
   const [users, setUsers] = useState(userData);
 
+  const addUser = user => {
+    user.id = users.length + 1;
+    users = setUsers([...users, user]);
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with React Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser}/>
         </div>
         <div className="flex-large">
           <h2>View users</h2>
---
+++ src/forms/AddUserForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const AddUserForm = props => {
+    const initialFormState = { id: null, name: '', username: ''};
+    const [user, setUser] = useState(initialFormState);
+
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        console.log(value);
+        console.log({[name]: value});
+        setUser({ ...user, [name]: value})
+    }
+
+    return (
+        <form>
+            <label>Name</label>
+            <input type="text" name="name" value={user.name} onChange={handleInputChange}/>
+            <label>Username</label>
+            <input type="text" name="username" value={user.username} onChange={handleInputChange}/>
+            <button>Add new user</button>
+        </form>
+    )
+}
+
+
+export default AddUserForm;
